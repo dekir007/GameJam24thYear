01c16c80707728cdf3bbb4fe2d1eea1102994014
Dekir <dekir0007@gmail.com>
2023-12-29 11:20:38 +0300

    added strange snowman dashing

--- game/scenes/Enemies/snow_man.gd
+++ game/scenes/Enemies/snow_man.gd
@@ -8,15 +8,36 @@ extends CharacterBody3D
 @onready var health_component:  = $HealthComponent as HealthComponent
 @onready var over_head: Marker3D = $OverHead
 
+var can_dash = true
+var dashing = false
+
 func _ready() -> void:
 	target = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta: float) -> void:
 	var dir = global_position.direction_to(target.global_position)
-	velocity = dir * speed * delta
+	#velocity = dir * speed * delta
+	var vel = dir * speed * delta
+	
+	velocity = lerp(velocity, vel, delta*10)
+	#velocity = velocity.move_toward(vel, vel.length_squared()/30)
+	var dist = global_position.distance_to(target.global_position) 
+	#print(self, dist)
+	if dist > 5 and dist < 6:
+		dash()
 	
 	look_at(target.global_position)
 	move_and_slide()
+
+func dash():
+	if can_dash:
+		velocity *= 4
+		dashing = true
+		can_dash = false
+		await get_tree().create_timer(.3).timeout
+		dashing = false
+		await get_tree().create_timer(1.7).timeout
+		can_dash = true
 
 func get_hit():
 	health_component.apply_damage(2)
